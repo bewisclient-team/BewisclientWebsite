01c73b3d7e9715729fbbddc7bf1b206f577fa704
Bewis09 <99420254+Bewis09@users.noreply.github.com>
2025-10-09 21:12:45 +0200

    Add 'has_elytra' field to cosmetic data selection

--- backend/cape.ts
+++ backend/cape.ts
@@ -75,7 +75,7 @@ export async function loadSpecialData() {
 }
 
 export async function loadCosmeticData() {
-    const { data } = await supabase.from('cosmetic_data').select('id, type, frames, default, old_id')
+    const { data } = await supabase.from('cosmetic_data').select('id, type, frames, default, old_id, has_elytra')
     
     if(data == null) return []
 
